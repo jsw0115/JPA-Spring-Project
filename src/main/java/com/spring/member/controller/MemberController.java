051683d74cd0241b2b2d248d55070d3998e8991c
jsw0115 <87688825+jsw0115@users.noreply.github.com>
2023-05-12 22:57:01 +0900

    ADD TEST

---
+++ src/main/java/com/spring/member/controller/MemberController.java
@@ -0,0 +1,109 @@
+package com.spring.member.controller;
+
+import com.spring.member.dto.MemberDTO;
+import com.spring.member.service.MemberService;
+import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/member")
+public class MemberController {
+
+    // 생성자 주입
+    private final MemberService memberService;
+
+    // 회원가입 페이지 출력 요청
+    @GetMapping("/save")
+    public String saveForm() {
+        return "member/memberSave";
+    }
+
+    @PostMapping("/save")
+    public String save (@ModelAttribute MemberDTO memberDTO) {
+        try {
+            System.out.println("MemberController.save");
+            System.out.println("memberDTO : " + memberDTO);
+            memberService.save(memberDTO);
+        } catch (Exception e) {
+            // 중복된 이메일인 경우 알림창 띄우기 등의 처리
+            return "member/duplicateEmail";
+        }
+        return "member/login";
+    }
+
+    @GetMapping("/login")
+    public String loginForm() {
+        return "member/login";
+    }
+
+    @PostMapping("/login")
+    public String login(@ModelAttribute MemberDTO memberDTO, HttpSession session ) {
+        MemberDTO loginResult = memberService.login(memberDTO);
+        if (loginResult != null) {
+            // login 성공
+            session.setAttribute("loginEmail", loginResult.getMemberEmail());
+            session.setAttribute("loginName", loginResult.getMemberName());
+            return "main";
+        } else {
+            // login 실패
+            return "member/login";
+        }
+    }
+
+    @GetMapping("/")
+    public String findAll (Model model) {
+        List<MemberDTO> memberDTOList = memberService.findAll();
+        // 어떠한 HTML로 가져갈 데이터가 있다면 model 사용
+        model.addAttribute("memberList", memberDTOList);
+        return "member/memberList";
+    }
+
+    @GetMapping("/{id}")
+    public String findById (@PathVariable Long id, Model model) {
+        MemberDTO memberDTO = memberService.findById(id);
+        model.addAttribute("member", memberDTO);
+        return "member/memberDetail";
+    }
+
+    @GetMapping("/update")
+    public String updateForm (HttpSession session, Model model) {
+        String myEmail = (String) session.getAttribute("loginEmail");
+        MemberDTO memberDTO = memberService.updateForm(myEmail);
+        model.addAttribute("updateMember", memberDTO);
+        return "member/memberUpdate";
+    }
+
+    @PostMapping("/update")
+    public String update (@ModelAttribute MemberDTO memberDTO) {
+        memberService.update(memberDTO);
+        return "redirect:/member/" + memberDTO.getId();
+    }
+
+    @GetMapping("/delete/{id}")
+    public String deleteById (@PathVariable Long id) {
+        memberService.deleteById(id);
+        return "redirect:/member/";
+    }
+
+    @GetMapping("/logout")
+    public String logout (HttpSession session) {
+        session.invalidate();
+        return "index";
+    }
+    @RequestMapping (value = "/member/email-check", method = RequestMethod.POST)
+    public @ResponseBody String emailCheck (@RequestParam("memberEmail") String memberEmail) {
+        System.out.println("memberEmail = " + memberEmail);
+        String checkResult = memberService.emailCheck(memberEmail);
+        if (checkResult != null) {
+            return "ok";
+        } else {
+            return "no";
+        }
+    }
+}
---
+++ src/main/java/com/spring/member/entity/MemberEntity.java
@@ -0,0 +1,41 @@
+package com.spring.member.entity;
+
+import com.spring.member.dto.MemberDTO;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Setter
+@Getter
+@Table(name = "member_table")
+public class MemberEntity {
+    @Id // pk 지정
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // auto_increment
+    private Long id;
+
+    @Column(unique = true)
+    private String memberEmail;
+
+    @Column
+    private String memberPassword;
+
+    @Column
+    private String memberName;
+
+    public static MemberEntity toMemberEntity (MemberDTO memberDTO) {
+        MemberEntity memberEntity = new MemberEntity();
+        memberEntity.setMemberEmail(memberDTO.getMemberEmail());
+        memberEntity.setMemberName(memberDTO.getMemberName());
+        memberEntity.setMemberPassword(memberDTO.getMemberPassword());
+        return memberEntity;
+    }
+
+    public static MemberEntity toUpdateMemberEntity (MemberDTO memberDTO) {
+        MemberEntity memberEntity = new MemberEntity();
+        memberEntity.setId(memberDTO.getId());
+        memberEntity.setMemberEmail(memberDTO.getMemberEmail());
+        memberEntity.setMemberName(memberDTO.getMemberName());
+        memberEntity.setMemberPassword(memberDTO.getMemberPassword());
+        return memberEntity;
+    }
+}
